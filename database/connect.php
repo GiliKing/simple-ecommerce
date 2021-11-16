124da500bf7d634ff6a60695270ca7069331c1c8
Ogili Christian <chrisogili12@gmail.com>
2021-11-16 15:22:30 -0800

    first commit

--- database/connect.php
+++ database/connect.php
@@ -2,10 +2,10 @@
 
 // Establish a connection to the database
 
-$host = "localhost";
-$user = "root";
-$db_password = "";
-$db_name = "ecom"; 
+$host = "us-cdbr-east-04.cleardb.com";
+$user = "b83eb6a7323267";
+$db_password = "83f87287";
+$db_name = "heroku_6d88d2afe7c6962"; 
 
 
 $conn = mysqli_connect($host, $user, $db_password, $db_name) or die ("could not connect");
