1fdb82b115d7283a6d7044e0140beafe05e85b36
Ogili Christian <chrisogili12@gmail.com>
2021-11-16 17:47:01 -0800

    first commit

--- index.php
+++ index.php
@@ -37,14 +37,14 @@
 </div>
 
 <nav class="navbar navbar-expand-lg navbar-light nav_ok">
-  <a class="navbar-brand" href="#" id="a_col"><h2>Maverick Meal</h2></a>
+  <a class="navbar-brand" href="index.php" id="a_col"><h2>Maverick Meal</h2></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#" id="a_col">Home<span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="index.php" id="a_col">Home<span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="login.php" id="a_col">Place Order</a>
